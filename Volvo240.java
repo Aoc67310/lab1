c11915323f073775ab21be1f6ea441b9e659c39c
theoblennardussart <142418157+theoblennardussart@users.noreply.github.com>
2023-11-01 09:51:05 +0100

    test

--- Volvo240.java
+++ Volvo240.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 
 public class Volvo240{
-// hej
-    //heike
+
+    
     public final static double trimFactor = 1.25;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
