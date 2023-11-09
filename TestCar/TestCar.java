b3a23ae82dbaaf57039172ee9ea4a2498ef8a878
Aoc67310 <lucas.hayhanen@live.se>
2023-11-09 15:41:55 +0100

    TestCar.java made for all tests. changed to assertEquals.

--- TestCar/TestCar.java
+++ TestCar/TestCar.java
@@ -4,8 +4,7 @@ import java.awt.*;
 import java.util.Objects;
 
 import static java.awt.Color.RED;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestCar{
 
@@ -15,37 +14,40 @@ public class TestCar{
     // Car.java
 
     @Test
-    public void TestGetNrOfDoors() {assertTrue(Objects.equals(saab.getNrDoors(), 2)); }
+    public void TestGetNrOfDoors() {
+        assertEquals(2, saab.getNrDoors()); }
     @Test
     public void testGetModelName(){
-    assertTrue(Objects.equals(saab.modelName, "Saab95"));
+        assertEquals("Saab95", saab.modelName);
     }
 
     @Test
-    public void TestGetEnginePowerSaab95(){assertTrue(saab.getEnginePower() == 125);}
+    public void TestGetEnginePowerSaab95(){
+        assertEquals(125, saab.getEnginePower(), 0.0);}
 
     @Test
-    public void TestGetColor(){assertTrue(Objects.equals(saab.getColor(), RED));}
+    public void TestGetColor(){
+        assertEquals(saab.getColor(), RED);}
 
     @Test
     public void TestSetColor() {
         saab.setColor(Color.red);
-        assertTrue(Objects.equals(Color.red, saab.color));}
+        assertEquals(Color.red, saab.color);}
 
     @Test
     public void TestStartEngine() {
         saab.startEngine();
-        assertTrue(Objects.equals(saab.currentSpeed, 0.1)); }
+        assertEquals(0.1, saab.currentSpeed, 0.0); }
 
     @Test
     public void TestGetCurrentSpeed() {
         saab.startEngine();
-        assertTrue(Objects.equals(saab.getCurrentSpeed(), saab.currentSpeed));
+        assertEquals(saab.getCurrentSpeed(), saab.currentSpeed, 0.0);
     }
     @Test
     public void TestStopEngine() {
         saab.stopEngine();
-        assertTrue(Objects.equals(saab.currentSpeed, 0.0));
+        assertEquals(0.0, saab.currentSpeed, 0.0);
     }
 
     @Test
@@ -53,56 +55,57 @@ public class TestCar{
         double beforecordx = saab.xkoordinat;
         double beforecordy = saab.ykoordinat;
         saab.move();
-        assertTrue(Objects.equals(saab.xkoordinat, beforecordx + saab.currentSpeed));
-        assertTrue(Objects.equals(saab.ykoordinat, beforecordy));
+        assertEquals(saab.xkoordinat, beforecordx + saab.currentSpeed, 0.0);
+        assertEquals(saab.ykoordinat, beforecordy, 0.0);
         beforecordx = saab.xkoordinat;
         beforecordy = saab.ykoordinat;
         saab.direction = 90;
         saab.move();
-        assertTrue(Objects.equals(saab.ykoordinat, beforecordy + saab.currentSpeed));
-        assertTrue(Objects.equals(saab.xkoordinat, beforecordx));
+        assertEquals(saab.ykoordinat, beforecordy + saab.currentSpeed, 0.0);
+        assertEquals(saab.xkoordinat, beforecordx, 0.0);
         beforecordx = saab.xkoordinat;
         beforecordy = saab.ykoordinat;
         saab.direction = 180;
         saab.move();
-        assertTrue(Objects.equals(saab.xkoordinat, beforecordx - saab.currentSpeed));
-        assertTrue(Objects.equals(saab.ykoordinat, beforecordy));
+        assertEquals(saab.xkoordinat, beforecordx - saab.currentSpeed, 0.0);
+        assertEquals(saab.ykoordinat, beforecordy, 0.0);
         beforecordx = saab.xkoordinat;
         beforecordy = saab.ykoordinat;
         saab.direction = 270;
         saab.move();
-        assertTrue(Objects.equals(saab.ykoordinat, beforecordy - saab.currentSpeed));
-        assertTrue(Objects.equals(saab.xkoordinat, beforecordx));
+        assertEquals(saab.ykoordinat, beforecordy - saab.currentSpeed, 0.0);
+        assertEquals(saab.xkoordinat, beforecordx, 0.0);
     }
 
     @Test
     public void TestTurnRight() {
         int beforedirection = saab.direction;
         saab.turnRight();
-        assertTrue(Objects.equals(saab.direction, (beforedirection + 90) % 360));
+        assertEquals(saab.direction, (beforedirection + 90) % 360);
     }
 
     @Test
     public void TestTurnLeft() {
         int beforedirection = saab.direction;
         saab.turnLeft();
-        assertTrue(Objects.equals(saab.direction, (beforedirection - 90) % 360));
+        assertEquals(saab.direction, (beforedirection - 90) % 360);
     }
 
     //Volvo240
     @Test
-    public void TestVolvo240SpeedFactor() {assertTrue(Objects.equals(volvo.speedFactor(), volvo.enginePower * 0.01 * 1.25)); }
+    public void TestVolvo240SpeedFactor() {
+        assertEquals(volvo.speedFactor(), volvo.enginePower * 0.01 * 1.25, 0.0); }
     @Test
     public void TestVolvo240IncrementSpeed() {
         double beforespeed = volvo.currentSpeed;
         volvo.incrementSpeed(1.0);
-        assertTrue(Objects.equals(volvo.currentSpeed, Math.min(beforespeed + volvo.speedFactor()* volvo.currentSpeed, volvo.speedFactor()))); }
+        assertEquals(volvo.currentSpeed, Math.min(beforespeed + volvo.speedFactor() * volvo.currentSpeed, volvo.speedFactor()), 0.0); }
 
     @Test
     public void TestVolvo240DecrementSpeed(){
         double beforespeed = volvo.currentSpeed;
         volvo.decrementSpeed(1.0);
-        assertTrue(Objects.equals(volvo.currentSpeed, Math.max(beforespeed - volvo.speedFactor() * 1,0)));
+        assertEquals(volvo.currentSpeed, Math.max(beforespeed - volvo.speedFactor() * 1, 0), 0.0);
     }
 
     //Saab95
@@ -124,28 +127,28 @@ public class TestCar{
     public void TestSaab95speedFactorTurboOff() {
         Saab95 Saab = (Saab95) saab;
         Saab.setTurboOff();
-        assertTrue(Objects.equals(Saab.speedFactor(), Saab.enginePower * 0.01));
+        assertEquals(Saab.speedFactor(), Saab.enginePower * 0.01, 0.0);
     }
 
     @Test
     public void TestSaab95speedFactorTurboOn() {
         Saab95 Saab = (Saab95) saab;
         Saab.setTurboOn();
-        assertTrue(Objects.equals(Saab.speedFactor(), Saab.enginePower * 0.01 * 1.3));
+        assertEquals(Saab.speedFactor(), Saab.enginePower * 0.01 * 1.3, 0.0);
     }
 
     @Test
     public void TestSaab95incrementSpeed() {
         double beforespeed = saab.currentSpeed;
         saab.incrementSpeed(1.0);
-        assertTrue(Objects.equals(saab.currentSpeed, beforespeed + saab.speedFactor() * 1));
+        assertEquals(saab.currentSpeed, beforespeed + saab.speedFactor() * 1, 0.0);
     }
 
     @Test
     public void TestSaab95decrementSpeed() {
         double beforespeed = saab.currentSpeed;
         saab.decrementSpeed(1.0);
-        assertTrue(Objects.equals(saab.currentSpeed, beforespeed - saab.speedFactor() * 1));
+        assertEquals(saab.currentSpeed, beforespeed - saab.speedFactor() * 1, 0.0);
     }
 }
 
