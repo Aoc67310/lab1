7f417ba221d4c80d798864bbbaea9308c8a440c6
Aoc67310 <lucas.hayhanen@live.se>
2023-12-10 16:25:53 +0100

    Changes by theo

--- CarFactory.java
+++ CarFactory.java
@@ -5,12 +5,8 @@ public class CarFactory {
     public Car createSaab95() {
         return (new Saab95());
     }
-
-    public Car createVolvo240(){
-
-        Car car = new Volvo240() {
-        };
-        return car;
+    public Car creatvolvo240(){
+        return new Volvo240();
 
     }
 
